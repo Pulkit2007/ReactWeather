fb9a674197f71e1eb9d341ece22e29fc823c0581
Pulkit2007 <bansalpulkit2007@outlook.com>
2026-09-15 19:32:09 +0530

    intial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+
+export default function Card(){
+
+    return(
+        <div className="weather-container">
+            <h1>Today</h1>
+            <h3 className="weather-type">Sunny and Bright</h3>
+            <h3 className="humidity">Humidity</h3>
+
+        </div>
+    )
+}
